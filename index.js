e99e4c71741188f38b15e35198a4c6917226229b
thinkchen <thinkchen@tencent.com>
2017-09-11 14:20:40 +0800

    fix deepcopy bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jscalpel = ({ target, keys, prefix, callback, deep, dynamicKeys, plugins},
     const deepCopy = (obj) => {
         const returnObj = {};
         let tempArr = [];
-        if (nativeToString.call(obj) === ['object Object']) {
+        if (nativeToString.call(obj) === '[object Object]') {
             Object.keys(obj).forEach((key, index) => {
                 if (Array.isArray(obj[key])) {
                     obj[key].forEach((value, index) => {
@@ -34,9 +34,8 @@ const jscalpel = ({ target, keys, prefix, callback, deep, dynamicKeys, plugins},
     try {
         epTarget = typeof target === 'string' ? JSON.parse(target) : target;
         if (deep) {
-            alert('isDeepCopy');
             epTarget = deepCopy(epTarget);
-        }
+        } 
         if (!nativeToString.call(epTarget) === '[object Object]') {
             console.error('传入的target不是有效的json或者object');
             return;
